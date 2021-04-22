8506754f293bb724ea2bf06ada2210095d92da8a
oagnouba <oagnouba@gmail.com>
2021-04-22 10:23:18 +0200

    Create index.js
    - retrieval of items from the server;
    - put the items in the form of a list

---
+++ front-end/index.js
@@ -0,0 +1,28 @@
+fetch("http://localhost:3000/api/teddies/")
+
+.then(response => { return response.json() })
+
+.then(data => {
+
+    console.log(data)
+
+    //afficher sur la page html
+    let listOfProducts = '';
+    data.forEach(prod =>
+        listOfProducts += `
+      <div class='col'>
+        <div class="card">
+          <img src=${prod.imageUrl} class="card-img-top">
+          <div class="card-body"></div>
+            <h3 class="card-title" ><a href="article.html?id=${prod._id}">${prod.name}</a></h3>
+            <p class="card-text">${prod.price}€</p>
+          </div>
+      </div>
+      `
+    );
+
+    document.getElementById('allproducts').innerHTML = listOfProducts;
+
+});
+
+
