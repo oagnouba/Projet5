ce520a6c3addef2e212becc8e18c61739a2b4e8b
oagnouba <oagnouba@gmail.com>
2021-04-30 00:53:30 +0200

    Update article.js
    - add an alert message with catch for fetch;
    - change the code to add item to basket;
    - add an alert message when item is added to basket

--- front-end/article.js
+++ front-end/article.js
@@ -3,18 +3,18 @@ let productId = urlParam.get("id");
 let product = {};
 
 fetch("http://localhost:3000/api/teddies/" + productId)
-  .then((response) => {
-    return response.json();
-  })
+    .then((response) => {
+        return response.json();
+    })
 
-  .then((data) => {
+.then((data) => {
     console.log(data);
     product = data;
 
     let colors = "";
 
     for (const color of data.colors) {
-      colors += `<option>${color}</option> `; //sélection de couleurs
+        colors += `<option>${color}</option> `; //sélection de couleurs
     }
 
     let container = `
@@ -32,23 +32,40 @@ fetch("http://localhost:3000/api/teddies/" + productId)
     document.getElementById("product").innerHTML = container;
 
     console.log(container);
-  });
+})
+
+.catch(function (error) {
+  window.alert('Le serveur n\'est pas disponible, essayez ultérieurement.');
+});
 
 //Ajout panier
 function ajouterAuPanier() {
-  if (localStorage.getItem(productId) === null) {
-    // si non présent dans le panier
-    product.quantity = 1;
-    let productLine = JSON.stringify(product);
-    localStorage.setItem(productId, productLine);
-  } // produit déja présent, modifier quantité
-  else {
-    let itemPanier = localStorage.getItem(productId); //récuperation
-    let productPanier = JSON.parse(itemPanier); //conversion
 
-    productPanier.quantity += 1; //initialiser qté = 1
+    let basket = GetBasket();
 
-    let productLine = JSON.stringify(productPanier);
-    localStorage.setItem(productId, productLine);
-  }
+    //https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/TypedArray/findIndex
+    let indexInBasket = basket.findIndex(item => item._id === productId)
+
+    //index ==-1 signifie non trouvé
+    if (indexInBasket < 0) {
+        // si non présent dans le panier
+        product.quantity = 1;
+        // let productLine = JSON.stringify(product);
+        // localStorage.setItem(productId, productLine);
+        basket.push(product);
+    } // produit déja présent, modifier quantité
+    else {
+        // let itemPanier = localStorage.getItem(productId); //récuperation
+        // let productPanier = JSON.parse(itemPanier); //conversion
+
+        // productPanier.quantity += 1; //initialiser qté = 1
+
+        // let productLine = JSON.stringify(productPanier);
+        // localStorage.setItem(productId, productLine);
+        basket[indexInBasket].quantity += 1;
+    }
+
+    SaveBasket(basket);
+    window.alert("Votre article a été ajouté au panier.");
+
 }
